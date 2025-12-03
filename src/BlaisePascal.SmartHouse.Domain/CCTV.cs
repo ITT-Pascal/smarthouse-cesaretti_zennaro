6933477cf2249b91229fee4b92278053e43250c6
Morgan-Zennaro <morgan.zennaro.stud@ispascalcomandini.it>
2025-12-03 11:00:40 +0100

    added CCTV and CCTVValidator

---
+++ src/BlaisePascal.SmartHouse.Domain/CCTV.cs
@@ -0,0 +1,31 @@
+﻿using BlaisePascal.SmartHouse.Domain.Asbtraction;
+using BlaisePascal.SmartHouse.Domain.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    public class CCTV : AbstractDevice
+    {
+        public bool IsRecording { get; set; }
+        public float ZoomValue {  get; set; }
+        public float Rotation {  get; set; }
+        public CCTV(string name) : base(name)
+        {
+            ZoomValue = 1;
+            Rotation = 0;
+            IsRecording = false;
+        }
+        public void Rotate(float degrees)
+        {
+            Rotation = CCTVValidator.RotationValidator(Rotation + degrees);
+        }
+        public void Zoom(float zoom)
+        {
+            ZoomValue = CCTVValidator.ZoomValidator(ZoomValue + zoom);
+        }
+    }
+}
---
+++ src/BlaisePascal.SmartHouse.Domain/Validator/CCTVValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.Validator
+{
+    public static class CCTVValidator
+    {
+        public const float maxRotationDegrees = 90;
+        public const float minRotationDegrees = -90;
+        public const float maxZoom = 10;
+        public const float minZoom = 0.5f;
+        public static float RotationValidator (float rotation)
+        {
+            float newRotation = 0;
+            if (rotation < minRotationDegrees)
+                newRotation = minRotationDegrees;
+            else if (rotation > maxRotationDegrees)
+                newRotation = maxRotationDegrees;
+            else
+                newRotation = rotation;
+            return newRotation;
+        }
+        public static float ZoomValidator (float zoom)
+        {
+            float newZoom = 0;
+            if (zoom < minZoom)
+                newZoom = minZoom;
+            else if (zoom > maxZoom)
+                newZoom = maxZoom;
+            else
+                newZoom = zoom;
+            return zoom;
+        }
+    }
+}
