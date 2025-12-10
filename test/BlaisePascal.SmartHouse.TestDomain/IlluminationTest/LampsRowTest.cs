33c1ba245b99056115349004fbed9759d2b46750
Morgan-Zennaro <morgan.zennaro.stud@ispascalcomandini.it>
2025-12-10 10:57:44 +0100

    remove ecoLampTest from LampTest

--- test/BlaisePascal.SmartHouse.TestDomain/IlluminationTest/LampsRowTest.cs
+++ test/BlaisePascal.SmartHouse.TestDomain/IlluminationTest/LampsRowTest.cs
@@ -2,7 +2,6 @@
 using BlaisePascal.SmartHouse.Domain.Asbtraction;
 using BlaisePascal.SmartHouse.Domain.ObjectStatus;
 using BlaisePascal.SmartHouse.Domain.Illumination;
-using BlaisePascal.SmartHouse.Domain.Illumination;
 using System;
 using System.Collections.Generic;
 using System.Linq;
